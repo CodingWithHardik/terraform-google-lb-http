80049f3e98200470dabe6bff0b8c2e39ddb05f01
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2024-08-26 18:17:55 +0530

    Update main.tf

--- examples/multi-backend-multi-mig-bucket-https-lb/main.tf
+++ examples/multi-backend-multi-mig-bucket-https-lb/main.tf
@@ -110,10 +110,10 @@ locals {
     port         = 80
   }
 }
-create_ssl_certificate = true
-managed_ssl_certificate_domains = ["example.com"]
 # [START cloudloadbalancing_ext_http_gce_plus_bucket]
 module "gce-lb-https" {
+  create_ssl_certificate = true
+  managed_ssl_certificate_domains = ["example.com"]
   source  = "terraform-google-modules/lb-http/google"
   version = "~> 10.0"
   name    = var.network_name
